07e9fa3458a055e652ac82ffec6f21efe50fdbfc
Neltarim <kyurann@gmail.com>
2020-08-21 21:01:16 +0200

    transition fade working nicely

--- am_front_static/js/am_custom.js
+++ am_front_static/js/am_custom.js
@@ -1,32 +1,33 @@
 function fadeIn( elem, ms )
 {
-  if( ! elem )
-    return;
- 
-  elem.style.opacity = 0;
-  elem.style.filter = "alpha(opacity=0)";
-  elem.style.display = "inline-block";
-  elem.style.visibility = "visible";
- 
-  if( ms )
-  {
-    var opacity = 0;
-    var timer = setInterval( function() {
-      opacity += 50 / ms;
-      if( opacity >= 1 )
-      {
-        clearInterval(timer);
-        opacity = 1;
-      }
-      elem.style.opacity = opacity;
-      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
-    }, 50 );
-  }
-  else
-  {
-    elem.style.opacity = 1;
-    elem.style.filter = "alpha(opacity=1)";
-  }
+
+    if( ! elem )
+        return;
+    
+    elem.style.opacity = 0;
+    elem.style.filter = "alpha(opacity=0)";
+    elem.style.display = "inline-block";
+    elem.style.visibility = "visible";
+    
+    if( ms )
+    {
+        var opacity = 0;
+        var timer = setInterval( function() {
+        opacity += 50 / ms;
+        if( opacity >= 1 )
+        {
+            clearInterval(timer);
+            opacity = 1;
+        }
+        elem.style.opacity = opacity;
+        elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+        }, 50 );
+    }
+    else
+    {
+        elem.style.opacity = 1;
+        elem.style.filter = "alpha(opacity=1)";
+    }
 }
  
 function fadeOut( elem, ms )
@@ -60,33 +61,27 @@ function fadeOut( elem, ms )
 }
 
 
+
 function show_section(section) {
     var welcome = document.getElementById("welcome");
     var section_to_show = document.getElementById(section);
 
 
-    if(section == "welcome" && welcome.style.display == "none") { /* if the user want to show the welcome section */
-        fadeIn(welcome, 200);
-        setTimeout(() => {welcome.style.display = "inline";}, 199);
-        return 0; /* stop here to cancel the next instruction (important) */
-    }
 
     if (welcome.style.display != "none" && section != "welcome") { /* Turn off welcome display */
         fadeOut(welcome, 200);
-        setTimeout(() => {welcome.style.display = "none";}, 199);
     } else {    /* if another section is already open */
         var sections = document.getElementsByClassName("central-div");
 
         for(var i=0; i < sections.length; i++) {
-            if (sections[i].style.display == "inline") {
-                fadeOut(sections[i], 200)
-                setTimeout(() => {sections[i].style.display = "none";}, 199);
+            if (sections[i].style.display != "none" && sections[i].id != section) {
+                fadeOut(sections[i], 180)
             }
         }
     }
 
     try { /* now try to display the section to open */
-        section_to_show.style.display = "inline";
+        setTimeout(() => {fadeIn(section_to_show, 200);}, 270);
     } catch (error) {
         console.log(error);
     }
